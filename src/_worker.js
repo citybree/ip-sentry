10ad1233d593578d5abe410c9e227ecbc551a0a5
蜗牛 <city9558c@gmail.com>
2026-07-18 23:27:27 +0800

    Update _worker.js

--- src/_worker.js
+++ src/_worker.js
@@ -2319,7 +2319,7 @@ function renderHtml(initData) {
                 <div className="flex items-center gap-2">
                    <StylePresetToggle value={stylePreset} onChange={setStylePreset} />
                    <ThemeToggle />
-                    <a href="https://github.com/jy02739244/ip-query-worker" target="_blank" rel="noreferrer" className="icon-button p-2.5 text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white transition">
+                    <a href="https://github.com/citybree/ip-sentry" target="_blank" rel="noreferrer" className="icon-button p-2.5 text-slate-500 hover:text-slate-900 dark:text-slate-300 dark:hover:text-white transition">
                        <Github className="w-5 h-5" />
                     </a>
                  </div>
